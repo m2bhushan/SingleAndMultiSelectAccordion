21940ef7b7ccf26448c8a2f29b08f63df8a65dff
m2bhushan <bhushanmani4017@gmail.com>
2023-12-22 22:26:51 +0530

    commit2

---
+++ accordion/src/components/accordion/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        id: '1',
+        question : " what are accordion components",
+        answer: " Accordion components are user interace elements sued for organizing data elements sued for organizing data"
+    },
+    {
+        id: '2',
+        question : " what are they used for",
+        answer: " They are used in variety of context They are used in variety of context They are used in variety of context"
+    },{
+        id: '3',
+        question : " As a musical instruction",
+        answer: "Lorem ipsum, dolor sit amet consectetur dolor sit amet consecteturdolor sit amet consecteturdolor sit amet consectetur dolor sit amet consectetur"
+    },{
+        id: '4',
+        question : " accordion statement 4",
+        answer: " lorem statement 4 and its answer dolor sit amet consecteturdolor sit amet consecteturdolor sit amet consecteturdolor sit amet consectetur"
+    },{
+        id: '5',
+        question : " accordion statement5 and its value",
+        answer: " Lorem ipsumorrupti assumenda rem placeat sed! Pariatur, minima nesciunt  assumenda rem placeat sed! Pariatur, minima nesciu assumenda rem placeat sed! Pariatur, minima nesciu."
+    },
+];
+export default data;
---
+++ accordion/src/components/accordion/index.js
@@ -0,0 +1,64 @@
+import { useState } from "react"
+import data from "./data";
+import "./style.css";
+
+
+export default function Accordion(){
+
+    const [selected, setSelected] = useState(null);
+    const [enableMutliSelect, setEnableMutliSelect] = useState(false);
+    const [multiple, setMultiple]= useState([])
+
+   function handleSingleSelection (getCurrentId){
+    setSelected(getCurrentId === selected 
+        ?
+        null
+        : 
+        getCurrentId
+        );
+   }
+
+   function handleMultiSelection (getCurrentId){
+
+    let cpyMultiple = [...multiple];
+    const findIndexOfCureentId = cpyMultiple.indexOf(getCurrentId)
+
+    console.log(findIndexOfCureentId);
+    if (findIndexOfCureentId === -1) cpyMultiple.push(getCurrentId)
+    else cpyMultiple.splice(findIndexOfCureentId, 1)
+
+    setMultiple(cpyMultiple)
+   }
+
+   console.log(selected, multiple);
+    return <div className="wrapper">
+        <button onClick={
+            ()=> setEnableMutliSelect(!enableMutliSelect)}>
+                Enable Multi select accordion</button>
+        <div className="accordion">
+            {
+                data && data.length > 0 ?
+                data.map(dataItem=> <div className="item">
+                    
+                <div onClick={ 
+                    enableMutliSelect
+                     ? ()=> handleMultiSelection(dataItem.id)
+                     : ()=> handleSingleSelection(dataItem.id)
+                     }
+                    className="title"
+                    >
+                    <h3>{dataItem.question}</h3>
+                    <span>  + </span> </div>
+
+                    {selected === dataItem.id 
+                    ? 
+                     <div className="content"> {dataItem.answer} 
+                     </div>
+                    : null}
+                    </div>) 
+                    
+                    : <div> No Data found</div>
+            }
+        </div>
+    </div>
+}
